451743e81aef3fe9ff9bb2af49239b7b275ddc57
Jean <Jean@JEANPFL>
2024-10-03 10:05:26 -0500

    3/10 1

--- src/environments/environment.development.ts
+++ src/environments/environment.development.ts
@@ -8,10 +8,10 @@ export const environment = {
   //apiUrl: 'https://localhost:7234/api'
 
 
-    apiUrl: 'https://localhost:60493/api'
+    //apiUrl: 'https://localhost:60493/api'
 
     //apiUrl: 'http://sd-4154134-w.dattaweb.com/back/api'
-    //apiUrl: 'https://informesdrr.com/back/api'
+    apiUrl: 'https://informesdrr.com/back/api'
 };
 
 /*
